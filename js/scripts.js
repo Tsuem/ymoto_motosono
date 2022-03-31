a911a40c694c4dad6eee4b6fddb3e223b9bf3fee
tsuem <tsuemotop@gmail.com>
2022-03-31 17:34:08 -0300

    creacion del repositorio

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('#menu');
+
+let menu_hamburguesa = document.querySelector('#menu_hamburguesa');
+
+   menu_hamburguesa.addEventListener('click',function(){
+
+    menu.classList.toggle('menu_toggle');
+   })
